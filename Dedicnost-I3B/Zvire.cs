b90a1e780834e2ce8dc2a64d517b460f378455f3
SGrohman <23ib05_Grohman@spsjc.cz>
2025-12-03 11:34:56 +0100

    diddyblud5

---
+++ Dedicnost-I3B/Zvire.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dedicnost_I3B
+{
+    public  class Zvire(string jmeno, int vek) //konstruktor 
+    {
+        public virtual string VydejZvuk(string jmeno)
+        {
+
+            return ""; //string.Empty;
+        }
+    }
+}
